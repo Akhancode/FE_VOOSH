1a614dcc50b73a74b0d981b3984ec6e4c10f72dc
akhancode <akhan.code@gmail.com>
2024-11-12 23:15:50 +0530

    Navbar changed

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,12 @@
+// ErrorPage.js
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className="flex justify-center items-center h-full">
+            <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
